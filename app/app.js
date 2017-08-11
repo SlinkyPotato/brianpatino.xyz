632004c1b0030eaad260b7e16279c2cd90fad5ef
Amaredeus <patinobrian@gmail.com>
2017-08-10 22:36:41 -0400

    upgrade dep, prep production, bump version

--- app/app.js
+++ app/app.js
@@ -6,9 +6,10 @@ angular.module('resumeApp', [
     'resumeApp.home'
 ])
 
-.config(['$locationProvider', '$routeProvider',
-  function config($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-    $routeProvider.otherwise({redirectTo: '/'});
-  }]
-);
+.config(['$locationProvider', '$routeProvider', '$compileProvider',
+function config($locationProvider, $routeProvider, $compileProvider) {
+	$compileProvider.debugInfoEnabled(false); // set to false to disable debug for production
+	$compileProvider.commentDirectivesEnabled(false); // set false to disable parsing code in comments
+  $locationProvider.hashPrefix('!');
+  $routeProvider.otherwise({redirectTo: '/'});
+}]);

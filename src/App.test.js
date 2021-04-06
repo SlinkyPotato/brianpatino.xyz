47c9e09d8adfc1e2921a0671b4571244a88beefc
Brian Patino <patinobrian@gmail.com>
2021-04-05 23:45:27 -0400

    add github action and js test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Brian Patino exists', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElements = screen.getAllByText(/Brian Patino/i);
+  expect(nameElements).toBeDefined();
 });
